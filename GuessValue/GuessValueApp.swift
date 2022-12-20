d2a551e69e9b13e63b53d6effffbc4c8cd18062b
Sav1taRG <52829649+Sav1taRG@users.noreply.github.com>
2022-12-20 22:01:15 +0200

    Initial Commit

---
+++ GuessValue/GuessValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessValueApp.swift
+//  GuessValue
+//
+//  Created by Roman Golubinko on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
